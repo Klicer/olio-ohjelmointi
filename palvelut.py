4fcd44df9f590a28521bde474c185cefa41609a8
Klicer <51763508+Klicer@users.noreply.github.com>
2023-04-10 16:11:12 +0300

    Lisää class Palvelu

--- palvelut.py
+++ palvelut.py
@@ -4,3 +4,20 @@ class Asiakas:
 
     def luo_nro():
         pass
+
+
+class Palvelu:
+    def __init__(self):
+        pass
+
+    def luo_asiakasrivi(Asiakas):
+        pass
+
+    def lisaa_asiakas(Asiakas):
+        pass
+
+    def poista_asiakas(Asiakas):
+        pass
+
+    def tulosta_asiakkaat():
+        pass
