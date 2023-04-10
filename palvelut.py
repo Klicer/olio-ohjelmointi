2bad152ea4b8b1bdc1632edb16e066ce8e32aaad
Klicer <51763508+Klicer@users.noreply.github.com>
2023-04-10 21:31:27 +0300

    Lisää dokumentointia

--- palvelut.py
+++ palvelut.py
@@ -2,6 +2,23 @@ import random
 
 
 class Asiakas:
+    """
+    Konstruktori ottaa 2 args: nimi, ika
+
+    Asiakas luokka
+    - nimi (str): asiakkaan nimi.
+    - ika (int): asiakkaan ikä.
+    - asiakasnro (lista): kolmen merkkijonon luettelo
+
+    Metodit:
+    - luo_nro(): Luo uuden asiakasnumeron.
+    - set_nimi(nimi: string): Asettaa asiakkaan nimen.
+    - get_nimi(): Hakee asiakkaan nimen.
+    - set_ika(ika: int): Asettaa asiakkaan iän.
+    - get_ika(): Hakee asiakkaan iän.
+    - get_asiakasnumero(): Palauttaa asiakasnumeroa edustavan merkkijonon.
+    """
+
     def __init__(self, nimi, ika):
         self.nimi = nimi
         self.ika = ika
@@ -38,6 +55,20 @@ class Asiakas:
 
 
 class Palvelu:
+    """
+    Konstruktori ottaa 1 argumentin: tuotenimi
+
+    Palvelu luokka
+    - tuotenimi (str): Tuotteen nimi.
+    - asiakkaat (lista): Lista Asiakas-olioita.
+
+    Metodit:
+    - luo_asiakasrivi(asiakas): Luo asiakkaasta muotoiltun merkkijonon, jossa on hänen nimi, asiakasnumero ja ikä.
+    - lisaa_asiakas(asiakas): Lisää uuden Asiakas-olion asiakkaat listaan.
+    - poista_asiakas(asiakas): Poistaa Asiakas-olion asiakkaat listasta.
+    - tulosta_asiakkaat(): Tulostaa listan tämän palvelun asiakkaista.
+    """
+
     def __init__(self, tuotenimi):
         self.tuotenimi = tuotenimi
         self.asiakkaat = []
@@ -62,6 +93,20 @@ class Palvelu:
 
 
 class ParempiPalvelu(Palvelu):
+    """
+    Konstruktori ottaa 1 argumentin: tuotenimi
+
+    ParempiPalvelu luokka
+    - tuotenimi (str): Tuotteen nimi.
+    - asiakkaat (lista): Lista Asiakas-olioita.
+    - edut (lista): Palvelun edut.
+
+    Metodit:
+    - lisaa_etu(self, edu): Lisää uuden edun tähän palveluun.
+    - poista_etu(self, edu): Poistaa edun tästä palvelusta.
+    - tulosta_edut(self): Tulostaa tämän palvelun edut.
+    """
+
     def __init__(self, tuotenimi):
         super().__init__(tuotenimi)
         self.edut = []
